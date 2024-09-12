e88cb0b1631cb1b1bdbd0eb357134601a1d1c4e0
damata12310 <anderson.daamata@gmail.com>
2024-09-12 01:45:57 -0300

    aula 3 módulo 2

--- modulo_2_aula_1/react-primeiro-projeto/src/app/components/Circle.tsx
+++ modulo_2_aula_1/react-primeiro-projeto/src/app/components/Circle.tsx
@@ -1,13 +1,5 @@
 //O nome do componente deve ter o mesmo do arquivo!
 //De preferencia de letra maiúscula sempre!
-export const Square = () => {
-  return (
-    <div className="w-52 h-52 bg-orange-700 text-white">
-      Texto qualquer inspirador
-    </div>
-  );
-}
-
 export const Circle = () => {
   return (
     <div className="w-52 h-52 bg-orange-700 text-whiote rounded-full">
--- modulo_2_aula_1/react-primeiro-projeto/src/app/page.tsx
+++ modulo_2_aula_1/react-primeiro-projeto/src/app/page.tsx
@@ -1,4 +1,6 @@
-import { Square, Circle } from '@/app/components/Geo';
+import { Circle } from "./components/Circle";
+import { GeoForm } from "./components/GeoForm";
+import { Square } from "./components/Square";
 
 //function Page (){} também posso declarar um componente dessa maneira!
 const Page = () => {
@@ -7,8 +9,7 @@ const Page = () => {
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Algum outro texto</h3>
 
-      <Square />
-      <Circle />
+      <GeoForm/>
     </div>
   ); 
 }
