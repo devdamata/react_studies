c2c5686ddff1ac1174d590dacb54aca767694286
damata12310 <anderson.daamata@gmail.com>
2024-09-12 02:50:17 -0300

    aula 5 modulo 2

--- modulo_2_aula_1/react-primeiro-projeto/src/app/components/Person.tsx
+++ modulo_2_aula_1/react-primeiro-projeto/src/app/components/Person.tsx
@@ -1,15 +1,26 @@
+const getWeekday = (today: Date) => {
+  return new Intl.DateTimeFormat('pt-BR', {weekday: 'long'}).format(today);
+}
+
 export const Person = () =>{
+  const data = {
+    name: 'Elon Musk',
+    avatar: 'https://classic.exame.com/wp-content/uploads/2022/12/urn_newsml_afp.com_20221217_87c09c92-9fd4-4cfb-aedc-440b0d2a6c0c_ipad.jpg?ims=750x/filters:quality(75):format(webp)',
+    roles: ['CEO da Tesla', 'CEO da SpaceX']
+  }
+  const today: Date = new Date();
+
   return(
     <>
-      <h1>Elon Musk</h1>
+      <h1 style={{ color: 'red', fontSize: '30px' }}>{data.name} - {getWeekday(today)}</h1>
       <img 
-        src="https://classic.exame.com/wp-content/uploads/2022/12/urn_newsml_afp.com_20221217_87c09c92-9fd4-4cfb-aedc-440b0d2a6c0c_ipad.jpg?ims=750x/filters:quality(75):format(webp)" 
-        alt=""
+        src={data.avatar}
+        alt={data.name}
         className="w-40"
       />
       <ul>
-        <li>CEO da Tesla</li>
-        <li>CEO da SpaceX</li>
+        <li>{data.roles[0]}</li>
+        <li>{data.roles[1]}</li>
         <li>CEO da Porra toda!</li>
       </ul>
     </>
